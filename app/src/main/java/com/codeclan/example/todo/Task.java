5f1c23c1dabf62efa317e2dee008cb47fb0cf6f4
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2016-11-14 13:07:46 +0000

    managed to get date working

--- app/src/main/java/com/codeclan/example/todo/Task.java
+++ app/src/main/java/com/codeclan/example/todo/Task.java
@@ -1,5 +1,6 @@
 package com.codeclan.example.todo;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -10,23 +11,50 @@ public class Task {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mCompleted;
 
-    public Task(){
+    public Task()
+    {
         //Create an ID for the task
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
 
     // create getter and setters... (mId does not need setter)
-    public UUID getId() {
+    public UUID getId()
+    {
         return mId;
     }
 
-    public String getTitle() {
+    public String getTitle()
+    {
         return mTitle;
     }
 
-    public void setTitle(String title) {
+    public void setTitle(String title)
+    {
         this.mTitle = title;
+    }
+
+    public Date getDate()
+    {
+        return mDate;
+    }
+
+    public void setDate(Date date)
+    {
+        this.mDate = date;
+    }
+
+    public boolean isCompleted()
+    {
+        return mCompleted;
+    }
+
+    public void setCompleted(boolean completed)
+    {
+        this.mCompleted = completed;
     }
 }
--- app/src/main/java/com/codeclan/example/todo/TaskFragment.java
+++ app/src/main/java/com/codeclan/example/todo/TaskFragment.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 /**
@@ -19,6 +23,8 @@ public class TaskFragment extends Fragment
 
     private Task mTask;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mCompletedCheckBox;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -52,6 +58,20 @@ public class TaskFragment extends Fragment
             public void afterTextChanged(Editable s)
             {
                 // Blank on purpose...
+            }
+        });
+
+        mDateButton = (Button)v.findViewById(R.id.task_date);
+        mDateButton.setText(DateFormat.format("EEEE dd MMM yyyy", mTask.getDate()).toString());
+        mDateButton.setEnabled(false);
+
+        mCompletedCheckBox = (CheckBox)v.findViewById(R.id.task_completed);
+        mCompletedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+            mTask.setCompleted(isChecked);
             }
         });
 
