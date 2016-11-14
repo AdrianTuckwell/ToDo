eefeba937f9ca33dfc7a658f881144397eeca449
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2016-11-14 15:36:20 +0000

    added recylerview

--- app/src/main/java/com/codeclan/example/todo/TaskListFragment.java
+++ app/src/main/java/com/codeclan/example/todo/TaskListFragment.java
@@ -3,11 +3,15 @@ package com.codeclan.example.todo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
-
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
 
 /**
  * Created by user on 14/11/2016.
@@ -16,6 +20,8 @@ import android.view.ViewGroup;
 public class TaskListFragment extends Fragment
 {
     private RecyclerView mTaskRecyclerView;
+    private TaskAdapter mAdapter;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -23,6 +29,55 @@ public class TaskListFragment extends Fragment
         View view = inflater.inflate(R.layout.fragment_task_list, container, false);
         mTaskRecyclerView = (RecyclerView) view.findViewById(R.id.task_recycler_view);
         mTaskRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        updateUI();
         return view;
+    }
+
+    private void updateUI()
+    {
+        TaskList taskList = TaskList.get(getActivity());
+        List<Task> tasks = taskList.getTasks();
+        mAdapter = new TaskAdapter(tasks);
+        mTaskRecyclerView.setAdapter(mAdapter);
+    }
+
+    private class TaskHolder extends RecyclerView.ViewHolder
+    {
+        public TextView mTitleTextView;
+        public TaskHolder(View itemView)
+        {
+            super(itemView);
+            mTitleTextView = (TextView) itemView;
+        }
+    }
+
+    private class TaskAdapter extends RecyclerView.Adapter<TaskHolder>
+    {
+        private List<Task> mTasks;
+        public  TaskAdapter(List<Task> tasks)
+        {
+            mTasks = tasks;
+        }
+
+        @Override
+        public TaskHolder onCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new TaskHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(TaskHolder holder, int position)
+        {
+            Task task = mTasks.get(position);
+            holder.mTitleTextView.setText(task.getTitle());
+        }
+
+        @Override
+        public int getItemCount()
+        {
+            return mTasks.size();
+        }
     }
 }
